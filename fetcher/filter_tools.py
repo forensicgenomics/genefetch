eaeadd1fa8e9cb9438f477d3a5819fb1adefb394
minimops <minimops.noah@gmail.com>
2024-12-27 19:38:16 +0100

    docstrings for filter tools

--- fetcher/filter_tools.py
+++ fetcher/filter_tools.py
@@ -1,3 +1,24 @@
+# This file is part of the mitoTree project and authored by Noah Hurmer.
+#
+# Copyright 2024, Noah Hurmer & mitoTree.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""
+exclusion_filters.py
+
+This script provides functions to dynamically generate filters for record exclusion
+based on user-provided exclusion lists in global EXCLUSIONS_DIR,
+as well as combine them with predefined static filters from global_defaults.py.
+
+Functions:
+- build_exclusion_filters: Dynamically creates filters from exclusion files.
+- load_filters: Combines static filters with dynamically created filters.
+
+Author: Noah Hurmer (aka. minimops) as part of the mitoTree Project.
+"""
 
 import os
 
@@ -11,7 +32,12 @@ def build_exclusion_filters(exclusions_dir):
             "description": filename_without_ext,
             "fun": lambda record: record.id in <loaded_id_list>
         }
-    Returns a list of these dicts.
+
+    Args:
+        exclusions_dir (str): Path to the directory containing exclusion files.
+
+    Returns:
+        list[dict]: A list of filter dictionaries with `description` and `fun` keys.
     """
     exclusion_filters = []
 
@@ -39,8 +65,17 @@ def build_exclusion_filters(exclusions_dir):
     return exclusion_filters
 
 
-# combine static + dynamic filters:
 def load_filters():
+    """
+    Load and combine static and dynamically created filters.
+
+    Combines:
+        - Predefined static filters (`FILTERS`) from `global_defaults.py`.
+        - Dynamically generated filters from exclusion files in `EXCLUSIONS_DIR`.
+
+    Returns:
+        list[dict]: A combined list of static and dynamically generated filters.
+    """
     dynamic_filters = build_exclusion_filters(exclusions_dir=EXCLUSIONS_DIR)
     all_filters = FILTERS + dynamic_filters
     return all_filters
